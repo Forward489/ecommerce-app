5fe034bb74e2150ae7b6c72fba22f315dbb717c9
Glenn Steven Santoso <glennstevencg21@gmail.com>
2023-08-18 14:50:05 +0700

    feat: add basic related components

---
+++ src/components/CardWrapper.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="content-wrapper">
+    <div class="card">Baju</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardWrapper",
+};
+</script>
+
+<style scoped>
+.content-wrapper {
+  position: absolute;
+  /* background-color: rgba(0, 128, 0, 0.381); */
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.card {
+  max-height: 100%;
+  max-width: 100%;
+  background-color: white;
+  padding: 36px 48px;
+  box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/MainBody.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="wrapper">
+        <div class="main-bg" :style="bgColor">
+
+        </div>
+        <div class="white">
+
+        </div>
+        <CardWrapper />
+    </div>
+</template>
+
+<script>
+    import CardWrapper from "./CardWrapper.vue";
+    export default {
+        name: "MainBody",
+        components: {
+            CardWrapper
+        },
+        props: {
+            bgColor: {
+                type: String,
+                // default: "#FDE2FF"
+            }
+        },
+        computed: {
+            color(){
+                return {
+                    "background-color": this.bgColor
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    body{
+        margin: 0;
+        min-width: 100vw;
+        min-height: 100vh;
+    }
+    .wrapper{
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        position: relative;
+    }
+
+    .main-bg{
+        /* background: #FDE2FF; */
+        background-image: url('~@/assets/bg-pattern.png');
+        height: 65%;
+        width: 100%;
+    }
+
+    .white{
+        background-color: white;
+        height: 35%;
+        width: 100%;
+    }
+</style>
